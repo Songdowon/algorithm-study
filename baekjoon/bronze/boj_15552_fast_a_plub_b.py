e6a8b3d3f27f9b0ee0d7e35f5bcc37ee94336d76
Songdowon <a01041581684@gmail.com>
2026-03-18 00:11:42 +0900

    feat : BOJ 15552 빠른 A+B 해결

---
+++ baekjoon/bronze/boj_15552_fast_a_plub_b.py
@@ -0,0 +1,25 @@
+# BOJ 15552 - 빠른 A+B
+
+"""
+문제:
+입력 개수가 많아서 빠른 입출력 필요
+
+접근:
+sys.stdin.readline 사용
+
+시간복잡도:
+O(N)
+
+배운 점:
+input()보다 빠른 입출력 필요
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+number = int(input())
+
+for _ in range(number):
+    a, b = map(int, input().split())
+    print(a + b)
