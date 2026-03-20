8b254f9403fe1129ba45af2adf342028e94e3432
Songdowon <a01041581684@gmail.com>
2026-03-20 12:13:35 +0900

    update : python code structure

--- baekjoon/bronze/boj_15552_fast_a_plus_b.py
+++ baekjoon/bronze/boj_15552_fast_a_plus_b.py
@@ -1,25 +1,3 @@
-# BOJ 15552 - 빠른 A+B
-
-"""
-문제 요약:
-- 입력 개수가 많아 빠른 입출력이 필요한 문제
-
-핵심 아이디어:
-- input() 대신 sys.stdin.readline을 사용해 입력 속도를 개선
-
-사용한 것:
-- sys.stdin.readline
-- 반복문
-- map(int, input().split())
-
-주의할 점:
-- input() 사용 시 시간초과 가능
-- sys.stdin.readline은 개행 문자를 포함할 수 있음
-
-배운 점:
-- Python에서는 입력 크기가 클 경우 빠른 입출력이 중요하다
-"""
-
 import sys
 
 input = sys.stdin.readline
