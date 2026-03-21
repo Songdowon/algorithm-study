329e3045b218beec3df0273a8314cd69039314a0
Songdowon <a01041581684@gmail.com>
2026-03-21 11:22:38 +0900

    update: BOJ 10807 count

--- baekjoon/bronze/10807/solution.py
+++ baekjoon/bronze/10807/solution.py
@@ -1,12 +1,43 @@
 # BOJ 10807 - 개수 세기 
 
 import sys
-
 input = sys.stdin.readline
 
-n = int(input())
-arr = list(map(int, input().split()))
-target = int(input())
+#-------------------------
+# solution 1: count() 사용
+#-------------------------
 
-print(arr.count(target))
+def solution1(nums, target):
+
+    return nums.count(target)
+
+#-------------------------
+# solution 2: 직접 구현
+#-------------------------
+
+def solution2(n , nums, target):
+
+    count = 0
+
+    for i in range(n):
+        if target == nums[i]:
+            count += 1
+    
+    return count
+
+#-------------------------
+# main
+#-------------------------
+
+def main():
+    n = int(input())
+    nums = list(map(int, input().split()))
+    target = int(input())
+
+    # 1, 2 중 선택
+
+    result = solution2(n, nums, target)
+    print(result)
+
+main()
 
