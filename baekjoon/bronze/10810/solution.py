9304ecb1412fbefa0ca35a53cd61933f619a273c
Songdowon <a01041581684@gmail.com>
2026-03-22 23:26:08 +0900

    update: 10810 put ball in basket

--- baekjoon/bronze/10810/solution.py
+++ baekjoon/bronze/10810/solution.py
@@ -6,15 +6,22 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+def solution(n, operations):
 
-basket = [0]*n
+    basket = [0] * n
 
-for _ in range(m):
-    i, j, k = map(int, input().split())
-    for i in range(i-1, j):
-        basket[i] = k
+    for i, j, k in operations:
+        for idx in range(i-1, j):
+            basket[idx] = k
 
-print(*basket)
+    return basket
 
+def main():
+    n, m = map(int, input().split())
+    operations = [tuple(map(int, input().split())) for _ in range(m)]
+
+    result = solution(n, operations)
+    print(*result)
+
+main()
 
